93e50331c50910894ea413bbd4c1e347305d93fc
Richard Fitzgerald <richard.fitzgerald@zappistore.com>
2022-09-21 15:39:57 +0100

    Add make-unique command

---
+++ src/align.rs
@@ -0,0 +1,82 @@
+use clap::Parser;
+use indicatif::ProgressBar;
+use std::{collections::BTreeMap, error::Error};
+
+/// Align several csv by column name and concatenate them
+#[derive(Parser, Debug)]
+pub struct AlignArgs {
+    /// Only use columns that are in at least this many files
+    #[arg(default_value_t = 1, long)]
+    at_least: usize,
+    input_files: Vec<String>,
+    #[arg(short, long)]
+    out: String,
+    #[arg(short, long)]
+    verbose: bool,
+}
+
+pub fn csvalign(args: AlignArgs) -> Result<(), Box<dyn Error>> {
+    let headers = {
+        let mut headers: BTreeMap<String, usize> = BTreeMap::new();
+        for file in args.input_files.iter() {
+            let mut reader = csv::Reader::from_path(file)?;
+            for header in reader.headers()? {
+                let header = header.trim();
+                if let Some(count) = headers.get_mut(header) {
+                    *count += 1
+                } else {
+                    headers.insert(header.to_owned(), 1);
+                }
+            }
+        }
+        if args.verbose {
+            println!("Found {} columns", headers.len())
+        }
+        headers.retain(|_k, &mut v| v > args.at_least);
+        let mut headers = headers.into_keys().collect::<Vec<_>>();
+        headers.sort();
+        if args.verbose && args.at_least > 1 {
+            println!("{} columns left after filtering", headers.len())
+        }
+        headers
+    };
+
+    if headers.len() == 0 {
+        if args.verbose {
+            println!("Nothing to do, as csv would be empty");
+        }
+        return Ok(());
+    }
+
+    let mut writer = csv::Writer::from_path(args.out)?;
+    writer.write_record(&headers)?;
+
+    println!("Concatinating {} files", args.input_files.len());
+    let pb = ProgressBar::new(args.input_files.len() as u64);
+    for file in args.input_files.iter() {
+        let mut reader = csv::Reader::from_path(file)?;
+        let this_headers = reader.headers()?;
+        let header_locations: Vec<_> = {
+            headers
+                .iter()
+                .map(|header| this_headers.iter().position(|x| x.trim() == header))
+                .collect()
+        };
+        for file_row in reader.records() {
+            let row = file_row?;
+            for field in header_locations.iter() {
+                if let Some(loc) = field {
+                    writer.write_field(row.get(*loc).unwrap())?;
+                } else {
+                    writer.write_field("")?;
+                }
+            }
+            writer.write_record::<&[&str], _>(&[])?;
+        }
+        pb.inc(1);
+    }
+    pb.finish_and_clear();
+    println!("Done!");
+
+    Ok(())
+}
---
+++ src/uniq.rs
@@ -0,0 +1,88 @@
+use clap::Parser;
+use std::{collections::BTreeMap, error::Error};
+
+#[derive(Debug)]
+enum UniqType {
+    First,
+    Max(usize),
+    Min(usize),
+}
+
+/// Align several csv by column name and concatenate them
+#[derive(Parser, Debug)]
+pub struct MakeUniqArgs {
+    input_file: String,
+    #[arg(short, long)]
+    verbose: bool,
+    /// Column to ensure every entry is unique in
+    unique_column: String,
+
+    /// Choose which enty to keep by keeping the maximum value in this column
+    #[arg(long, group = "select")]
+    max_by: Option<String>,
+
+    /// Choose which enty to keep by keeping the minimum value in this column
+    #[arg(long, group = "select")]
+    min_by: Option<String>,
+
+    #[arg(short, long)]
+    out: String,
+}
+
+pub fn csvmakeuniq(args: MakeUniqArgs) -> Result<(), Box<dyn Error>> {
+    let mut reader = csv::Reader::from_path(args.input_file)?;
+    let mut writer = csv::Writer::from_path(args.out)?;
+    writer.write_record(reader.headers()?)?;
+    let this_headers = reader.headers()?;
+    let unique_col = this_headers
+        .iter()
+        .position(|x| x == args.unique_column)
+        .ok_or("Couldn't find column in input csv")?;
+
+    let select_type = if let Some(col) = args.max_by {
+        let pos = this_headers
+            .iter()
+            .position(|x| x.trim() == col)
+            .ok_or("Couldn't find max-by column in input csv")?;
+        UniqType::Max(pos)
+    } else if let Some(col) = args.min_by {
+        let pos = this_headers
+            .iter()
+            .position(|x| x.trim() == col)
+            .ok_or("Couldn't find min-by column in input csv")?;
+        UniqType::Min(pos)
+    } else {
+        UniqType::First
+    };
+    let mut known_entries: BTreeMap<String, csv::StringRecord> = BTreeMap::new();
+    for file_row in reader.records() {
+        let row = &file_row?;
+        let maybe_uniq = row.get(unique_col).unwrap();
+        if let Some(existing_row) = known_entries.get(maybe_uniq) {
+            match &select_type {
+                UniqType::First => {}
+                UniqType::Max(col) => {
+                    let new = row.get(*col).unwrap();
+                    let old = existing_row.get(*col).unwrap();
+                    if new > old {
+                        known_entries.insert(maybe_uniq.to_owned(), row.clone());
+                    }
+                }
+                UniqType::Min(col) => {
+                    let new = row.get(*col).unwrap();
+                    let old = existing_row.get(*col).unwrap();
+                    if new < old {
+                        known_entries.insert(maybe_uniq.to_owned(), row.clone());
+                    }
+                }
+            }
+        } else {
+            known_entries.insert(maybe_uniq.to_owned(), row.clone());
+        }
+    }
+    for record in known_entries.into_values() {
+        writer.write_record(&record)?;
+    }
+
+    Ok(())
+}
